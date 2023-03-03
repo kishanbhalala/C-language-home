0f126db9b97b052a95ab841e42d439225a2a17d0
kishan bhalala <kishanbhalala555@gmail.com>
2023-03-03 11:53:25 +0530

    home

---
+++ PROJECT/A65-Z90.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ char i;                 //A=65,B=66,C=67.....print
+  clrscr();
+
+ for(i=65; i<=90; i++)
+ {
+  printf("%c-%d\t",i,i);
+ }
+
+ getch();
+
+}
